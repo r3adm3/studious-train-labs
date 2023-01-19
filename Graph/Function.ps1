7ba7f9250e41d5da53ba9bb5b42b0c8e9300d185
Jag Bains <git>
2023-01-19 16:01:21 +0000

    Commit1

--- Graph/Function.ps1
+++ Graph/Function.ps1
@@ -1,9 +1,2 @@
-
-
-$firstNumber = 10
-
-$secondnumber = 10
-
-$addednumbers = $firstnumber + $secondnumber
-
-write-output "your numbers added toghther is: $addednumber"
+Connect-MgGraph
+Get-Mguser
