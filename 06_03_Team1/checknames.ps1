883af10be07b9a48d8ced00cfa0b243992a9c47a
Susanna <susanna@example.com>
2023-01-20 16:25:03 +0000

    update

--- 06_03_Team1/checknames.ps1
+++ 06_03_Team1/checknames.ps1
@@ -3,7 +3,7 @@ $namesArray =Import-Csv -Path 06_03_Team1\userlist2.csv
 $count = $namesArray.Count
 
 For ($name = 0; $name -lt $count; $name++ ) {
-    get-mguser -Filter "DisplayName eq '$name'"
+    get-mguser -Filter "DisplayName eq '$($namesArray[$name])'"
 }
   
 
